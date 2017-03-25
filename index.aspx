e5fb68200170dd4964c4efd8a6b9336fc0765aa2
bene2929 <benedikt.kantz@htl-klu.at>
2017-03-25 16:09:02 +0100

    Initial Release

---
+++ index.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Brazzifier</title>
+    <style>
+        body {
+            font-family: Arial;
+        }
+        #output {
+            width:50%;
+        }
+    </style>
+    <script type="text/javascript">
+        window.onunload = function (e) {
+            PageMethods.Closing_2(onSuccess,onFailure);
+        };
+        function onSuccess(e) {
+
+        }
+        function onFailure(e) {
+
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+    <asp:ScriptManager ID="scriptman" runat="server" EnablePageMethods="true">
+    </asp:ScriptManager>
+        <div>
+            <h1>Brazzifier</h1>
+            <h2>Brazzifiy your images with this simple Website!</h2>
+            <div id="legal">Brazzer is a Logo by the Brazzer company. This site isn't built to generate profit for me or anyone else, just for fun!</div><br />
+            <asp:FileUpload ID="image" runat="server" /><br />
+            <asp:Button ID="start" runat="server" Text="Start!" /><br />
+            <asp:Image ID="output" runat="server" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class index : System.Web.UI.Page
+{
+    [WebMethod]
+    public string Closing()
+    {
+        File.Delete(uniquePath());
+        return "Done!";
+    }
+    [WebMethod]
+    public static string Closing_2()
+    {
+        Console.WriteLine("ENDDD");
+        return "Done!";
+    }
+    private string ID;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        start.Click += Start_Click;
+        this.Unload += Index_Unload;
+        ID = Guid.NewGuid().ToString(); 
+    }
+
+    private void Index_Unload(object sender, EventArgs e)
+    {
+       // File.Delete(uniquePath());
+    }
+
+    private string uniqueName()
+    {
+        return ID + ".png";
+    }
+    private string uniquePath()
+    {
+        return Server.MapPath("/") + uniqueName();
+    }
+    private void Start_Click(object sender, EventArgs e)
+    {
+        System.Drawing.Bitmap bm = new System.Drawing.Bitmap(image.FileContent);
+        Bitmap newBitmap = new Bitmap(bm.Width, bm.Height);
+        Graphics graphics = Graphics.FromImage(newBitmap);
+        graphics.DrawImage(bm, 0, 0, newBitmap.Width,newBitmap.Height);
+        System.Drawing.Bitmap logo = new System.Drawing.Bitmap(Server.MapPath("/")+"logo.png");
+        float mult = 0.05f / ((float)logo.Height / (float)newBitmap.Height);
+        mult = float.IsInfinity(mult) ? 1 : mult;
+        graphics.DrawImage(logo, newBitmap.Width - logo.Width * mult-1, newBitmap.Height - logo.Height * mult-1, logo.Width * mult, logo.Height * mult);
+        newBitmap.Save(uniquePath());
+        output.ImageUrl = uniqueName();
+        
+
+    }
+}
